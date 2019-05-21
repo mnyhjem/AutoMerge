4078d487eae563eed02e1d16a0c17f3d9d5816a5
Martin Nyhjem <email@martinnyhjem.dk>
2019-05-21 17:26:35 +0200

    vsix ændret til 2019

--- src/AutoMerge/Resources.Designer.cs
+++ src/AutoMerge/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace AutoMerge {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -89,7 +89,7 @@ namespace AutoMerge {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to My recent changesets.
+        ///   Looks up a localized string similar to Merge candidates.
         /// </summary>
         internal static string RecentChangesetSectionName {
             get {
